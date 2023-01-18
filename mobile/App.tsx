64607c12739e6f9d9a26bcd8240f8fed1437d5dc
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-18 00:09:22 +0000

    mobile updated

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  });
+
+  if(!fontsLoaded) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>New App Coming Soon!! 🚀</Text>
--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -4,6 +4,6 @@ interface HabitProps {
 
 export function Habit (props : HabitProps) {
     return (
-        <div className="bg-zinc-900 w-10 text-white rounded m-2 flex items-center justify-center cursor-pointer">{props.completed}</div>
+        <div className="w-10 bg-purple-900 text-white rounded m-2 flex items-center justify-center cursor-pointer">{props.completed}</div>
     )
 }
