dc741665e6ab8b2c23c87b6e6a738d54660b61bf
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-19 01:26:54 +0000

    frontEndUpdated

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,17 @@
 import './styles/global.css'
-import { Plus } from 'phosphor-react'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 // import { Habit } from './components/Habit'
 
-import logoImage from './assets/logo.svg'
-import { Header } from './components/Header'
 
 export function App() {
 
   return (
    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex-col gap-16'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
         <Header />
-
-
+        <SummaryTable />
       </div>
    </div>
   )
--- web/src/components/Habit.tsx
+++
@@ -1,9 +0,0 @@
-interface HabitProps {
-    completed: number;
-}
-
-export function Habit (props : HabitProps) {
-    return (
-        <div className="bg-background w-10 text-white rounded m-2 flex items-center justify-center cursor-pointer">{props.completed}</div>
-    )
-}
