c1c264f97d61ca629f3ee240bcea71a57256173e
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-20 07:04:59 +0000

    frontend Updated

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -33,7 +33,13 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />
+          return (
+            <HabitDay 
+            amount={10} 
+            completed={Math.round(Math.random() * 10)} 
+            key={date.toString()} 
+            />
+          )
         })}
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
           return (
